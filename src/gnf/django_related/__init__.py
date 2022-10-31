ed7026df761dedf7bce0ffd05968dd4b9543e9ca
Jessica <jessica.lynn.millar@gmail.com>
2022-10-31 14:13:25 -0400

    change initial migration

--- src/gnf/django_related/__init__.py
+++ src/gnf/django_related/__init__.py
@@ -1,8 +1,7 @@
 """G Node Factory."""
-import gnf.django_related.settings as settings
-from gnf.django_related.models import BaseGNodeDb
-from gnf.django_related.models import BaseGNodeHistory
-from gnf.django_related.models import GpsPointDb
+# from gnf.django_related.models import BaseGNodeDb
+# from gnf.django_related.models import BaseGNodeHistory
+# from gnf.django_related.models import GpsPointDb
 
 
-__all__ = ["BaseGNodeDb", "BaseGNodeHistory", "GpsPointDb", "settings"]
+# __all__ = ["BaseGNodeDb"]
