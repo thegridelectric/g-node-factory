5eef6def8ac4dfa94f553baeb20773d7dabc1913
Lukasz Tymoszczuk <lukas@codeily.com>
2022-11-18 01:11:53 +0000

    add transfer logic to smart contract

--- src/gnf/smart_contract/demo.py
+++ src/gnf/smart_contract/demo.py
@@ -1,12 +1,3 @@
-from resources import (
-    getTemporaryAccount,
-    createDummyAsset,
-)
-from setup import getAlgodClient
-from util import (
-    getBalances,
-)
-
 from account import Account
 from operations import (
     create_app,
@@ -16,23 +7,32 @@ from operations import (
     transfer_deed_to_dest,
     get_application_address
 )
-from algosdk import encoding
+from resources import (
+    getTemporaryAccount,
+    createDummyAsset,
+)
+from setup import getAlgodClient
+from util import (
+    getBalances,
+)
 
 
-def print_balance(app_id: int, creator: Account, src: Account, dest: Account) -> None:
+def print_balance(app_id: int, creator: Account, src: Account, dest: Account, debug: bool = False) -> None:
     creator_balance = getBalances(client, creator.getAddress())
     print("===============================================")
     src_balance = getBalances(client, src.getAddress())
-    print("Creator account's balance", creator_balance, creator.getAddress())
-    print("Smart contract created with id=", app_id)
+    print("Creator account's balance", creator_balance, creator.getAddress() if debug else "")
+    print("Smart contract with id=", app_id)
 
     app_address = get_application_address(app_id)
     app_balance = getBalances(client, app_address)
     dest_balance = getBalances(client, dest.getAddress())
-    print(".Escrow account's balance", app_balance, app_address)
-    print(".Owner account's balance", src_balance, src.getAddress())
-    print(".Target account's balance", dest_balance, dest.getAddress())
+    print(".Escrow account's balance", app_balance, app_address if debug else "")
+    print(".Deed Owner account's balance", src_balance, src.getAddress() if debug else "")
+    print(".(GNodeFactory) Target account's balance", dest_balance, dest.getAddress() if debug else "")
     print("===============================================")
+    input("Press enter to continue")
+    print("Next step...")
 
 
 print("Demo start...")
@@ -44,7 +44,7 @@ target: Account = getTemporaryAccount(client)
 
 deed_amount = 1
 deed_id = createDummyAsset(client, deed_amount, owner)
-print("Deed id is", deed_id)
+print("A demo deed created, id=", deed_id)
 
 # Interact with smart contract here
 app_id = create_app(client, target, creator, owner, deed_id)
