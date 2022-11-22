b25e5f9c487e0144786a4b0b694a71636b5a72df
Jessica <jessica.lynn.millar@gmail.com>
2022-11-22 15:45:01 -0500

    mount gnf db in docker image

--- demo.py
+++ demo.py
@@ -70,7 +70,7 @@ print("")
 print("")
 time.sleep(2)
 
-cmd = f"docker run -p 8000:8000 --name gnf-api jessmillar/gnf:chaos__2647cc9__20221122"
+cmd = f"docker run -p 8000:8000 --name gnf-api jessmillar/gnf:chaos__ed7891f__20221122"
 gnf_pr = subprocess.Popen(cmd.split())
 api_endpoint = "http://0.0.0.0:8000/"
 gnf_up: bool = False
--- src/gnf/django_related/settings.py
+++ src/gnf/django_related/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "gnf.django_related.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "django_related/db.sqlite3"),
+        "NAME": os.path.join(BASE_DIR, "../../db.sqlite3"),
     },
 }
 
